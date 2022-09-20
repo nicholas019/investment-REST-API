88a1b943b1ecd63360a917d6abba51b97588c5cf
sorkihg <sorkihg@gmail.com>
2022-09-20 15:01:06 +0900

    Feat : 회원가입 및 로그인, 인증인가 기능 추가
    1. 본인의 자산 및 계좌정보를 조회하기위한 인증 및 인가기능 추가
     - simple-jwt 라이브러리를 활용한 token 발급기능 구현

--- api/v1/users/urls.py
+++ api/v1/users/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
-urlpatterns = [
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+from api.v1.users.views import SignUpView
+
+
+urlpatterns = [
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('sign-up', SignUpView.as_view())
 ]
--- config/settings/base.py
+++ config/settings/base.py
@@ -77,6 +77,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
 ]
 
